0450c9e77053541c10ce3458a929f72089e9e2f1
JaumPOS <joaopedroocana@gmail.com>
2026-06-17 08:12:29 -0300

    alteração do arquivo riscos/apps.py

--- risco_ufsm/riscos/apps.py
+++ risco_ufsm/riscos/apps.py
@@ -6,6 +6,12 @@ class RiscosConfig(AppConfig):
     '''configuração do app'''
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'riscos'
+    verbose_name = 'Alteração de Risco'
 
     def ready(self):
-        import riscos.signals #noqa
+        '''Registra os models relacionados à alteração de planos de risco.'''
+        signals_module = 'risco.signals'
+        try:
+            __import__(signals_module)
+        except ImportError:
+            pass
